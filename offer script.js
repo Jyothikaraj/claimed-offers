ab13b61bc245034f060e21d887dd0fb2b7f071f0
Jyothikaraj <112985341+Jyothikaraj@users.noreply.github.com>
2024-11-01 08:52:12 -0700

    Update offer script.js

--- offer script.js
+++ offer script.js
@@ -16,7 +16,7 @@ document.getElementById('claim-offer-btn').addEventListener('click', function() 
     formData.append('email', email);
   
     // Send the form data to the server using Google Apps Script
-    fetch('https://script.google.com/macros/s/AKfycbyJ2l0bqEfPm2O2AxDeAlmQ44QleA1IFQIZ3TSYunFTNmSMdkSF0EUZuKWCDI5PRFVz/exec', {
+    fetch('https://script.google.com/macros/s/AKfycbwyqXjluJMVHBuHgmThh6SaWUl1Lzg-eNFKqRlW74vQg760r6UCQuialPnNOZLPy0Hy/exec', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded'
