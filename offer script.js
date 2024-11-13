6099c661bd82a953dbdfeb0ffaa77389b7e3fde5
Jyothikaraj <112985341+Jyothikaraj@users.noreply.github.com>
2024-11-13 09:33:42 -0800

    Update offer script.js

--- offer script.js
+++ offer script.js
@@ -6,7 +6,7 @@ let timer;
 
 async function startOfferTimer(email) {
   try {
-    const response = await fetch(`https://script.google.com/macros/s/AKfycbxpBnsItPJz6AoU4yYmQT87cMjHaEY1FiXDZs13lgxIvtKLZpBbXNa_88qhKFaYZJvz/exec?action=getTimestamp&email=${encodeURIComponent(email)}`);
+    const response = await fetch(`https://script.google.com/macros/s/AKfycbztulCw-yPCh5oEemmln59RXDKqG-AXgtQhuTcklYDhR68Sl2rwP9Rxnmnxsj2L6wSB/exec?action=getTimestamp&email=${encodeURIComponent(email)}`);
     const result = await response.json();
     // Debug log to check the response result
     console.log("Fetch result:", result);
